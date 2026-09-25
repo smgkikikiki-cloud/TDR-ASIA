9cc706db748d766942a1a2b5e099aa6e3c76b8b1
smgkikikiki-cloud <smgkikikiki@gmail.com>
2026-09-25 19:17:53 +0700

    Add account page

---
+++ app/account/page.tsx
@@ -0,0 +1,5 @@
+import { AccountClient } from "@/components/account/AccountClient";
+
+export const metadata={title:"Account | TDR Asia",robots:{index:false,follow:false}};
+
+export default function AccountPage(){return <main className="page accountPage"><div className="shell"><AccountClient/></div></main>}
